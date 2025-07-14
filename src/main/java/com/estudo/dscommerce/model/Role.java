0066964b170aa99fda0c975ab8468d876c9f3e51
Lucas Honorio <lucashonorioo@hotmail.com>
2025-07-14 17:46:46 -0300

    Criando classe entity Role, adicionando relacionamento com User, e adicionando dependencias do spring security

---
+++ src/main/java/com/estudo/dscommerce/model/Role.java
@@ -0,0 +1,30 @@
+package com.estudo.dscommerce.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String authority;
+
+    public Role(){
+
+    }
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+}
